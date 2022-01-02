54aad1fcfc2ac6117912979e0af9b02a578ea38c
aoda <atsushi.oda.gml@gmail.com>
2022-01-03 01:35:02 +0900

    add detect_giraffe.py

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,12 @@ FROM ultralytics/yolov5:latest
 RUN pip uninstall -y pafy
 RUN pip install youtube_dl
 RUN pip install git+https://github.com/Cupcakus/pafy
+
+COPY detect_giraffe.py /usr/src/app/
+COPY detect_giraffe.sh /usr/src/app/
+
+ENV HOME=/usr/src/app
+ARG ALERT_URL
+ENV ALERT_URL=${ALERT_URL} 
+
+CMD [ "./detect_giraffe.sh" ]
---
+++ detect_giraffe.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Namibia: Live stream in the Namib Desert
+python detect_giraffe.py --nosave --source 'https://youtu.be/ydYDqZQpim8' --alert-url="${ALERT_URL}"
+# NamibiaCam: Giraffe visiting the waterhole 5 November 2021 (for test)
+# python detect_giraffe.py --nosave --source 'https://youtu.be/OBKluUO2jwU' --alert-url="${ALERT_URL}"
